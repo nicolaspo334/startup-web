6f807d48ed13b893484988d953d305836c656971
Nicolas Perez Olleta <nicolasperezolleta@MacBook-Air-de-Nicolas.local>
2026-01-16 18:44:21 +0100

    a

--- src/pages/user/UserSearch.tsx
+++ src/pages/user/UserSearch.tsx
@@ -292,8 +292,8 @@ export default function UserSearch() {
             {/* Map */}
             <MapContainer center={[40.416775, -3.703790]} zoom={13} style={{ width: "100%", height: "100%" }}>
                 <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 />
 
                 {filteredSpaces.map(space => {
